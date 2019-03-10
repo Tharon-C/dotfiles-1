3775bf2d9af6c6949e8f1b59523b7169edb29721
tharon-c <tharonica@gmail.com>
2019-03-09 18:08:43 -0700

    mnt to media

--- ubuntuSettings.sh
+++ ubuntuSettings.sh
@@ -1,8 +1,6 @@
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 24
 
 mkdir /media/usb
-chown $USER /media/
-chmod 0777 /media/usb
-cat >> /etc/fstab
-/dev/sdc1 /media/usb auto user,umask=000,utf8,noauto 0 0
-^D
+#chown $USER /media/
+#chmod 0777 /media/usb
+echo "/dev/sdc1 /media/usb auto user,umask=000,utf8,noauto 0 0" >> /etc/fstab
